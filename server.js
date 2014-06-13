1843872ac0efc38c01f3d7f2866bf6c8b8f257ed
Deephan <deephan@gmail.com>
2014-06-13 11:22:44 -0400

    More clean up

--- server.js
+++ server.js
@@ -5,36 +5,35 @@ var fs = require('fs');
 // For now, all these location have been hardcoded. 
 
 app.get('/weather/:id', function(req, res) {
-    	var http = require("https");
-    	var request = require('request');
-    	for(var item in req.query)
-    	console.log("Passing query param ==>  "+item+" : "+req.query[item]);
-		
-		// Configure the request
-		var options = {
-		    url: 'http://api.wunderground.com/api/0e59bfb8e72fd074/forecast/conditions/q/'+req.query.state+'/'+req.query.city+'.json',
-		    method: 'GET'
-		};
+	var http = require("https");
+	var request = require('request');
+	for(var item in req.query)
+	console.log("Passing query param ==>  "+item+" : "+req.query[item]);
 
-		// Start the request
-		request(options, function (error, response, body) {
-		    if (!error && response.statusCode == 200) {
-		        // Print out the response body
-		        var jsonObject = JSON.parse(body);
+	// Configure the request
+	var options = {
+	    url: 'http://api.wunderground.com/api/0e59bfb8e72fd074/forecast/conditions/q/'+req.query.state+'/'+req.query.city+'.json',
+	    method: 'GET'
+	};
 
-		        // Only showing limited fields are this point
-                var fields = ["icon","weather","temperature_string","relative_humidity","wind_string","pressure_mb","dewpoint_string","feelslike_string"];
-                var html = "";
-                html += '<table class="table"><thead><tr>';
-                for (var i = 0; i < fields.length; i++)   html+='<th align="center">  '+fields[i]+'   </th>';
-                html += "</td></thead><tbody><tr>";
-                for (var i = 0; i < fields.length; i++) 
-                    html += '<td align = "center"> '+jsonObject.current_observation[fields[i]]+"   </td>  ";
-                html += "</tr></tbody></table>";
-		        res.send(html);
-		    }
-		});
-	
+	// Start the request
+	request(options, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+			// Print out the response body
+			var jsonObject = JSON.parse(body);
+
+			// Only showing limited fields are this point
+			var fields = ["icon","weather","temperature_string","relative_humidity","wind_string","pressure_mb","dewpoint_string","feelslike_string"];
+			var html = "";
+			html += '<table class="table"><thead><tr>';
+			for (var i = 0; i < fields.length; i++)   html+='<th align="center">  '+fields[i]+'   </th>';
+			html += "</td></thead><tbody><tr>";
+			for (var i = 0; i < fields.length; i++) 
+			    html += '<td align = "center"> '+jsonObject.current_observation[fields[i]]+"   </td>  ";
+			html += "</tr></tbody></table>";
+			res.send(jsonObject.current_observation["forecast_url"]);
+		}
+	});
 });
 
 app.listen(3000);
