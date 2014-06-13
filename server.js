1785ee160d3bc0998eaeef33bb2a77dce25ab7b3
Deephan <deephan@gmail.com>
2014-06-13 14:10:33 -0400

    Adding new templates. All the pages will now be taken care by ejs rendering. Adding locations.json for reading the locations externally

--- server.js
+++ server.js
@@ -1,37 +1,54 @@
 var express = require('express'); 
 var app = express();
+var fs = require('fs');
 
-// Route for displaying the weather of each city.
-app.get('/weather/:id', function(req, res) {
+app.use('/public', express.static(__dirname + '/public'));
+
+app.all('*', function(req, res, next){
+	fs.readFile('locations.json', function(err, data){
+		res.locals.locations = JSON.parse(data);
+		next();
+	});
+});
+
+app.get('/weather', function(req, res){
+	res.render('index.ejs', res.locals.locations);
+});
+
+
+app.get('/api/locations', function(req, res){
+  res.json(res.locals.locations);
+});
+
+
+app.get('/weather/:city', function(req, res, next){
+
 	var http = require("https");
 	var request = require('request');
 	for(var item in req.query)
-	console.log("Passing query param ==>  "+item+" : "+req.query[item]);
-
+	console.log("Passing query parameter %s : %s", item , req.query[item]);
+	
 	// Configure the request
 	var options = {
 	    url: 'http://api.wunderground.com/api/0e59bfb8e72fd074/forecast/conditions/q/'+req.query.state+'/'+req.query.city+'.json',
-	    method: 'GET'
+		method: 'GET'
 	};
 
 	// Start the request
 	request(options, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
+		if (!error && response.statusCode == 200) {
 			// Print out the response body
 			var jsonObject = JSON.parse(body);
-
 			// Only showing limited fields are this point
 			var fields = ["icon","weather","temperature_string","relative_humidity","wind_string","pressure_mb","dewpoint_string","feelslike_string"];
-			var html = "";
-			html += '<table class="table"><thead><tr>';
-			for (var i = 0; i < fields.length; i++)   html+='<th align="center">  '+fields[i]+'   </th>';
-			html += "</td></thead><tbody><tr>";
-			for (var i = 0; i < fields.length; i++) 
-			    html += '<td align = "center"> '+jsonObject.current_observation[fields[i]]+"   </td>  ";
-			html += "</tr></tbody></table>";
-			res.send(html);
+			res.locals.locations.forEach(function(location){
+				if (req.query.city === location.city){
+				  res.render('location.ejs', { location: location, jsonObject : jsonObject, fields : fields });
+				}
+			})
 		}
 	});
+  
 });
 
 app.listen(3000);
